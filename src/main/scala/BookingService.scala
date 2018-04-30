4e9d52a4aaeddb9f5ed8e2c10af0fbd74e43d035
sameer garg <garg.sameer@gmail.com>
2018-04-30 14:38:25 +0100

    Introduce State Monad as the method return type

--- src/main/scala/BookingService.scala
+++ src/main/scala/BookingService.scala
@@ -8,59 +8,63 @@ class BookingService {
 
   // adds room to booking, stores an event
   def addRoom(
-    no: String,
-    floor: Int,
-    view: Boolean,
-    capacity: Int
-  ): Booking => (Booking, Room) = booking => {
+               no: String,
+               floor: Int,
+               view: Boolean,
+               capacity: Int
+             ): BookingState[Room] = State(booking => {
     val newRoom = RoomGenerator.generateRoom(no, floor, view, capacity)
     val newBooking = booking.copy(rooms = newRoom :: booking.rooms, events = RoomAdded(newRoom.no) :: booking.events)
     (newBooking, newRoom)
-  }
+  })
 
   // returns current reservation id
-  private def currentReservationId: Booking => (Booking, ReservationId) =
+  private def currentReservationId: BookingState[ReservationId] = State {
     booking => (booking, booking.rooms.flatMap(_.booked).map(_.id).foldLeft(0)(math.max(_, _)))
+  }
 
   // fetches room by number
-  def fetchRoom(no: String): Booking => (Booking, Option[Room]) = booking => (
-    booking.copy(events = RoomFetched(no) :: booking.events),
-    booking.rooms.filter(_.no == no).headOption
-  )
+  def fetchRoom(no: String): BookingState[Option[Room]] = State { booking =>
+    (booking.copy(events = RoomFetched(no) :: booking.events),
+      booking.rooms.filter(_.no == no).headOption)
+  }
 
   // books a guest to a room for a given period
   def book(
-    room: Room,
-    period: Period,
-    guest: Guest,
-    reservationId: ReservationId):Booking => (Booking, Unit) = booking => {
-    val roomReserved = room.copy(booked = Reservation(reservationId, period, guest) :: room.booked)
-    val otherRooms = booking.rooms.filter(_ != room)
-    val newBooking = booking.copy(
-      rooms = roomReserved :: otherRooms,
-      events = ReservationMade(reservationId) :: booking.events
-    )
-    (newBooking, ())
+            room: Room,
+            period: Period,
+            guest: Guest,
+            reservationId: ReservationId): BookingState[Unit] = State { booking => {
+      val roomReserved = room.copy(booked = Reservation(reservationId, period, guest) :: room.booked)
+      val otherRooms = booking.rooms.filter(_ != room)
+      val newBooking = booking.copy(
+        rooms = roomReserved :: otherRooms,
+        events = ReservationMade(reservationId) :: booking.events
+      )
+      (newBooking, ())
+    }
   }
 
   // book vor VIP = book given room, if it does not exist then build it
   def bookVip(
-    no: String,
-    floor: Int,
-    view: Boolean,
-    capacity: Int,
-    period: Period
-  )(guest: Guest): Booking => (Booking, ReservationId) = booking => {
-    val (fetchedRoomBooking, mayBeRoom: Option[Room]) = fetchRoom(no)(booking)
-    val (addedRoomBooking, newRoom) = mayBeRoom.fold {
-      addRoom(no, floor, view, capacity)(fetchedRoomBooking)
-    } {
-      room => (fetchedRoomBooking: Booking, room)
-    }
-    val resId = currentReservationId(addedRoomBooking)._2 + 1
-    val (newBooking, _) = book(newRoom, period, guest, resId)(addedRoomBooking)
-    (newBooking, resId)
-  }
+               no: String,
+               floor: Int,
+               view: Boolean,
+               capacity: Int,
+               period: Period
+             )(guest: Guest): Booking => (Booking, ReservationId) = ???
+
+  /*booking => {
+      val (fetchedRoomBooking, mayBeRoom: Option[Room]) = fetchRoom(no)(booking)
+      val (addedRoomBooking, newRoom) = mayBeRoom.fold {
+        addRoom(no, floor, view, capacity)(fetchedRoomBooking)
+      } {
+        room => (fetchedRoomBooking: Booking, room)
+      }
+      val resId = currentReservationId.run(addedRoomBooking).value._2 + 1
+      val (newBooking, _) = book(newRoom, period, guest, resId)(addedRoomBooking)
+      (newBooking, resId)
+    }*/
 
 }
 
