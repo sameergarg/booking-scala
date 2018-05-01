0ee3186a31879d81ccb5b8bdea643d82b4ca90cb
sameer garg <garg.sameer@gmail.com>
2018-05-01 14:04:29 +0100

    introduce monad transformer OptionT

--- src/main/scala/MonadTransformer.scala
+++ src/main/scala/MonadTransformer.scala
@@ -1,21 +1,21 @@
+import cats.data.OptionT
+
 import scala.util.{Failure, Success, Try}
 
 object MonadTransformer {
 
-  def calculate(input: String): Validation[Option[Int]] = Try(input.toInt) match {
-    case Success(i) => Right(Some(i +1))
-    case Failure(ex) => Left(ex.getMessage)
-  }
+  def calculate(input: String): Validation[Int] = increment(input)
 
-  val increment: Option[Int] = {
-    val incremented = for {
-      inc <- calculate("some")
-    } yield {
-      inc
+  val increment: String => Validation[Int] = str => {
+    def parseToInt(i: String): Either[String, Option[Int]] = Try(i.toInt) match {
+      case Success(i) => Right(Some(i+1))
+      case Failure(ex) => Left(ex.getMessage)
     }
-    incremented.fold(_ => None, identity)
+    OptionT[Error, Int](parseToInt(str))
   }
 
 
-  type Validation[A] = Either[String, A]
+  type Error[V] = Either[String, V]
+  type Validation[A] = OptionT[Error, A]
+
 }
