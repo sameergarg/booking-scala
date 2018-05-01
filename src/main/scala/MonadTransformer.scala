4c5267c091369cdad38c2f57c10b8c24cc9ad92f
sameer garg <garg.sameer@gmail.com>
2018-05-01 11:57:14 +0100

    Simple implementation without using monad transformer

--- src/main/scala/MonadTransformer.scala
+++ src/main/scala/MonadTransformer.scala
@@ -1,9 +1,20 @@
+import scala.util.{Failure, Success, Try}
 
 object MonadTransformer {
 
-  def calculate(input: String): Validation[Option[Int]] = ???
+  def calculate(input: String): Validation[Option[Int]] = Try(input.toInt) match {
+    case Success(i) => Right(Some(i +1))
+    case Failure(ex) => Left(ex.getMessage)
+  }
 
-  val increment: Option[Int] = ???
+  val increment: Option[Int] = {
+    val incremented = for {
+      inc <- calculate("some")
+    } yield {
+      inc
+    }
+    incremented.fold(_ => None, identity)
+  }
 
 
   type Validation[A] = Either[String, A]
